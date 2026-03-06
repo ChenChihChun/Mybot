6f796c1d394691aa826f92535177151ed22bfc30
Peter <maxpeter2100@gmail.com>
2026-03-07 01:29:18 +0800

    v2.0: fix commit message display

--- push.sh
+++ push.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd ~/githup/Mybot
-MSG="${*:-update}"
+
+if [ $# -gt 0 ]; then
+  MSG="$*"
+else
+  echo -n "Commit message: "
+  read MSG
+  MSG="${MSG:-update}"
+fi
+
 git add -A
 git commit -m "$MSG"
 git push origin main
